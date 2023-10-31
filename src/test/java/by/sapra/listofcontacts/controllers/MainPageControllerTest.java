47dae67dc09e729fbeb31db57ea23ec6dd719d4f
Евгений Сапрунов <sapra10062018@gmail.com>
2023-10-31 14:07:52 +0300

    изменено название теста на более подходящее

--- src/test/java/by/sapra/listofcontacts/controllers/MainPageControllerTest.java
+++ src/test/java/by/sapra/listofcontacts/controllers/MainPageControllerTest.java
@@ -11,7 +11,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
-public class ContactControllerTest {
+public class MainPageControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
