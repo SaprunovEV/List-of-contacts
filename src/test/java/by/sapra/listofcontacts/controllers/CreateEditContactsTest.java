3f2e64b54e1e88a85f610c4ce605f936c4a2d8b1
Евгений Сапрунов <sapra10062018@gmail.com>
2023-11-01 13:07:47 +0300

    переименован контроллер в связи с тем, что он теперь тестирует так же и создание контакта

--- src/test/java/by/sapra/listofcontacts/controllers/CreateEditContactsTest.java
+++ src/test/java/by/sapra/listofcontacts/controllers/CreateEditContactsTest.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
-public class EditDeleteContactsTest {
+public class CreateEditContactsTest {
 
     @Autowired
     MockMvc mockMvc;
