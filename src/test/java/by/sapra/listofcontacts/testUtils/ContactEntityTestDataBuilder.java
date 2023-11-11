ca151ad3a6853dc143b10ba6c287114725d7fed5
Евгений Сапрунов <sapra10062018@gmail.com>
2023-11-11 15:12:02 +0300

    исправление: выброшен ломбок из билдера, не компилировался нет времени решать

--- src/test/java/by/sapra/listofcontacts/testUtils/ContactEntityTestDataBuilder.java
+++ src/test/java/by/sapra/listofcontacts/testUtils/ContactEntityTestDataBuilder.java
@@ -1,11 +1,7 @@
 package by.sapra.listofcontacts.testUtils;
 
 import by.sapra.listofcontacts.model.ContactEntity;
-import lombok.AllArgsConstructor;
-import lombok.With;
 
-@With
-@AllArgsConstructor
 public class ContactEntityTestDataBuilder implements TestDataBuilder<ContactEntity> {
     private String firstName = "test_first";
     private String lastName = "test_last";
@@ -13,6 +9,13 @@ public class ContactEntityTestDataBuilder implements TestDataBuilder<ContactEnti
     private String phone = "test_phone";
 
     private ContactEntityTestDataBuilder() {}
+
+    public ContactEntityTestDataBuilder(String firstName, String lastName, String email, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
 
     public static ContactEntityTestDataBuilder aContact() {
         return new ContactEntityTestDataBuilder();
@@ -26,5 +29,21 @@ public class ContactEntityTestDataBuilder implements TestDataBuilder<ContactEnti
                 .email(email)
                 .phone(phone)
                 .build();
+    }
+
+    public ContactEntityTestDataBuilder withFirstName(String firstName) {
+        return this.firstName == firstName ? this : new ContactEntityTestDataBuilder(firstName, this.lastName, this.email, this.phone);
+    }
+
+    public ContactEntityTestDataBuilder withLastName(String lastName) {
+        return this.lastName == lastName ? this : new ContactEntityTestDataBuilder(this.firstName, lastName, this.email, this.phone);
+    }
+
+    public ContactEntityTestDataBuilder withEmail(String email) {
+        return this.email == email ? this : new ContactEntityTestDataBuilder(this.firstName, this.lastName, email, this.phone);
+    }
+
+    public ContactEntityTestDataBuilder withPhone(String phone) {
+        return this.phone == phone ? this : new ContactEntityTestDataBuilder(this.firstName, this.lastName, this.email, phone);
     }
 }
