c7fed17ecd78137c7dad86a8a6f5f7fd742ae477
Евгений Сапрунов <sapra10062018@gmail.com>
2023-11-14 13:20:49 +0300

    сделан зелёным тест сборки контекста(сейчас на псевдо базу)

--- src/test/java/by/sapra/listofcontacts/TestListOfContactsApplication.java
+++ src/test/java/by/sapra/listofcontacts/TestListOfContactsApplication.java
@@ -13,7 +13,7 @@ public class TestListOfContactsApplication {
 	@Bean
 	@ServiceConnection
 	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:12.3"));
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:14.5"));
 	}
 
 	public static void main(String[] args) {
