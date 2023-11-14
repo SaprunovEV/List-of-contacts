a85b0e2fe47a0e62f4c1fc57ddfb24f39e07ab3b
Евгений Сапрунов <sapra10062018@gmail.com>
2023-11-14 13:15:54 +0300

    настроен запуск базы через докер

---
+++ docker/init.sql
@@ -0,0 +1,9 @@
+CREATE SCHEMA IF NOT EXISTS contact_schema;
+
+CREATE TABLE IF NOT EXISTS contact_schema.contact (
+    id BIGINT primary key,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    email VARCHAR(30),
+    phone VARCHAR(30)
+);
